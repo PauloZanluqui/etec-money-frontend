2d60b2cd7dc503952c6e9863c58c991cc697abb2
Paulo Zanluqui <paulozanluqui13@gmail.com>
2024-10-31 20:08:09 -0300

    feat: Implementadas funcionalidades de pesquisa, filtros e paginação

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const baseUrl = "http://localhost:3333";
 let loadingSpinner;
 let overlay;
 
+const params = new URLSearchParams({
+  order: "asc",
+});
+
 function openModal(isEdit) {
-  if(isEdit) {
+  if (isEdit) {
     document.getElementById("Modal-Title").innerHTML = "Editar Transação";
     document.getElementById("button-save").style.display = "none";
     document.getElementById("button-edit").style.display = "block";
-  }
-  else {
-    document.getElementById("Modal-Title").innerHTML = "Adicionar Transação"
+  } else {
+    document.getElementById("Modal-Title").innerHTML = "Adicionar Transação";
     document.getElementById("button-save").style.display = "block";
     document.getElementById("button-edit").style.display = "none";
   }
@@ -23,8 +26,11 @@ function closeModal() {
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
-  loadingSpinner = document.getElementById('loading');
-  overlay = document.getElementById('overlay');
+  loadingSpinner = document.getElementById("loading");
+  overlay = document.getElementById("overlay");
+
+  tableSearch();
+
   await refreshInformations();
 });
 
@@ -35,8 +41,8 @@ async function refreshInformations() {
 
 async function getSummary() {
   await fetch(`${baseUrl}/transactions/summary`)
-   .then((response) => response.json())
-   .then((data) => {
+    .then((response) => response.json())
+    .then((data) => {
       document.getElementById("total-income").textContent = `R$ ${data.summary.totalCredit}`;
       document.getElementById("total-expense").textContent = `R$ ${data.summary.totalDebit}`;
       document.getElementById("balance").textContent = `R$ ${data.summary.netBalance}`;
@@ -51,69 +57,80 @@ function clearFields() {
 }
 
 async function listTransactions() {
-  overlay.style.display = 'block';
-  loadingSpinner.style.display = 'block';
+  overlay.style.display = "block";
+  loadingSpinner.style.display = "block";
   const tbody = document.getElementById("table-body");
 
   tbody.innerHTML = "";
 
-  await fetch(`${baseUrl}/transactions`)
-  .then((response) => response.json())
-  .then((data) => {
-    data.transactions.forEach((transaction) => {
-      if (transaction.type === "debit") {
-        const newRow = `
+  await fetch(`${baseUrl}/transactions?${params.toString()}`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.transactions.forEach((transaction) => {
+        const dateObj = new Date(transaction.createdAt);
+
+        const formattedDate = dateObj.toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        });
+
+        if (transaction.type === "debit") {
+          const newRow = `
         <tr id="${transaction.id}" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <td class="px-6 py-4">${transaction.id}</td>
                 <th class="px-6 py-4 font-medium text-gray-900 dark:text-white">${transaction.title}</th>
                 <td class="px-6 py-4 text-red-400">R$ ${transaction.amount}</td>
+                <td class="px-6 py-4">${formattedDate}</td>
                 <td class="px-6 py-4 text-right">
                   <a onclick="listOneTransaction(${transaction.id})" class="font-medium text-blue-600 cursor-pointer dark:text-blue-500 hover:underline pe-3">Editar</a>
                   <a onclick="deleteTransaction(${transaction.id})" class="font-medium text-blue-600 cursor-pointer dark:text-blue-500 hover:underline">Excluir</a>
                 </td>
               </tr>
         `;
-  
-        tbody.innerHTML += newRow;
-      }
-      else {
-        const newRow = `
+
+          tbody.innerHTML += newRow;
+        } else {
+          const newRow = `
         <tr id="${transaction.id}" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <td class="px-6 py-4">${transaction.id}</td>
                 <th class="px-6 py-4 font-medium text-gray-900 dark:text-white">${transaction.title}</th>
                 <td class="px-6 py-4 text-green-400">R$ ${transaction.amount}</td>
+                <td class="px-6 py-4">${formattedDate}</td>
                 <td class="px-6 py-4 text-right">
                   <a onclick="listOneTransaction(${transaction.id})" class="font-medium text-blue-600 cursor-pointer dark:text-blue-500 hover:underline pe-3">Editar</a>
                   <a onclick="deleteTransaction(${transaction.id})" class="font-medium text-blue-600 cursor-pointer dark:text-blue-500 hover:underline">Excluir</a>
                 </td>
               </tr>
         `;
-  
-        tbody.innerHTML += newRow;
-      }
-    });
-  })
-  .catch((error) => console.error("Erro: " + error));
-  overlay.style.display = 'none';
-  loadingSpinner.style.display = 'none';
+
+          tbody.innerHTML += newRow;
+        }
+      });
+
+      renderPagination(data.page, data.totalPages);
+    })
+    .catch((error) => console.error("Erro: " + error));
+  overlay.style.display = "none";
+  loadingSpinner.style.display = "none";
 }
 
 async function listOneTransaction(id) {
-  overlay.style.display = 'block';
-  loadingSpinner.style.display = 'block';
-  
-  await fetch(`${baseUrl}/transactions/${id}`)
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("itemId").value = data.transaction.id;
-    document.getElementById("title").value = data.transaction.title;
-    document.getElementById("amount").value = data.transaction.amount;
-    document.getElementById("type-transaction").value = data.transaction.type;
-  })
-  .catch((error) => console.error("Erro: " + error));
+  overlay.style.display = "block";
+  loadingSpinner.style.display = "block";
 
-  loadingSpinner.style.display = 'none';
-  overlay.style.display = 'none';
+  await fetch(`${baseUrl}/transactions/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("itemId").value = data.transaction.id;
+      document.getElementById("title").value = data.transaction.title;
+      document.getElementById("amount").value = data.transaction.amount;
+      document.getElementById("type-transaction").value = data.transaction.type;
+    })
+    .catch((error) => console.error("Erro: " + error));
+
+  loadingSpinner.style.display = "none";
+  overlay.style.display = "none";
 
   openModal(true);
 }
@@ -139,7 +156,7 @@ async function saveTransaction() {
 }
 
 async function editTransaction() {
-  let id =  document.getElementById("itemId").value;
+  let id = document.getElementById("itemId").value;
   let transaction = {
     title: document.getElementById("title").value,
     amount: Number(document.getElementById("amount").value),
@@ -163,8 +180,101 @@ async function deleteTransaction(id) {
   await fetch(`${baseUrl}/transactions/${id}`, {
     method: "DELETE",
   })
-   .then(closeModal())
-   .catch((error) => console.error("Erro: " + error));
+    .then(closeModal())
+    .catch((error) => console.error("Erro: " + error));
 
-   await refreshInformations();
-} 
+  await refreshInformations();
+}
+
+// Função para renderizar a paginação com estilo específico
+function renderPagination(currentPage, totalPages) {
+  const paginationDiv = document.getElementById("pagination");
+  paginationDiv.innerHTML = "";
+
+  // Botão "Anterior"
+  const prevLi = document.createElement("li");
+  const prevButton = document.createElement("a");
+  prevButton.innerHTML = "<";
+  prevButton.className = "flex items-center justify-center px-3 h-8 ms-0 text-gray-400 bg-gray-800 border border-gray-700 rounded-s-lg cursor-pointer hover:bg-gray-700 hover:text-white";
+  prevButton.onclick = (event) => {
+    event.preventDefault();
+    if (currentPage > 1) {
+      params.set("page", currentPage - 1);
+      refreshInformations();
+    }
+  };
+  if (currentPage === 1) prevButton.classList.add("opacity-50", "pointer-events-none");
+  prevLi.appendChild(prevButton);
+  paginationDiv.appendChild(prevLi);
+
+  // Botões das páginas
+  for (let i = 1; i <= totalPages; i++) {
+    const pageLi = document.createElement("li");
+    const pageButton = document.createElement("a");
+    pageButton.textContent = i;
+    pageButton.className = `flex items-center justify-center px-3 h-8 ${i === currentPage ? "text-white bg-gray-700" : "text-gray-400 bg-gray-800 hover:bg-gray-700 cursor-pointer hover:text-white"} border border-gray-700`;
+    pageButton.onclick = (event) => {
+      event.preventDefault();
+      params.set("page", i);
+      refreshInformations();
+    };
+    pageLi.appendChild(pageButton);
+    paginationDiv.appendChild(pageLi);
+  }
+
+  // Botão "Próximo"
+  const nextLi = document.createElement("li");
+  const nextButton = document.createElement("a");
+  nextButton.innerHTML = ">";
+  nextButton.className = "flex items-center justify-center px-3 h-8 text-gray-400 bg-gray-800 border border-gray-700 rounded-e-lg cursor-pointer hover:bg-gray-700 hover:text-white";
+  nextButton.onclick = (event) => {
+    event.preventDefault();
+    if (currentPage < totalPages) {
+      params.set("page", currentPage + 1);
+      refreshInformations();
+    }
+  };
+  if (currentPage === totalPages) nextButton.classList.add("opacity-50", "pointer-events-none");
+  nextLi.appendChild(nextButton);
+  paginationDiv.appendChild(nextLi);
+}
+
+function applyFilter(filterType) {
+  toggleDropdown();
+  if (filterType !== "all") {
+    params.set("type", filterType);
+  } else {
+    params.delete("type");
+  }
+  refreshInformations();
+}
+
+function toggleDropdown() {
+  document.getElementById("dropdown").classList.toggle("hidden");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".inline-flex")) {
+    const dropdown = document.getElementById("dropdown");
+    if (!dropdown.classList.contains("hidden")) {
+      dropdown.classList.add("hidden");
+    }
+  }
+};
+function tableSearch() {
+  document.getElementById("table-search").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      const title = event.target.value.trim();
+
+      if (title !== "") {
+        params.set("title", title);
+      } else {
+        params.delete("title");
+      }
+      refreshInformations();
+    } else if (event.key === "Backspace" && event.target.value == "") {
+      params.delete("title");
+      refreshInformations();
+    }
+  });
+}
